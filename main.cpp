9e2042c232d0e63961a853519426f2fc3ddf7f43
paswd <paswd@yandex.ru>
2016-11-17 23:44:47 +0300

    Modified

--- main.cpp
+++ main.cpp
@@ -7,13 +7,32 @@
 
 using namespace std;
 
+int CharToInt(char sym) {
+	int res = 0;
+	if (sym >= '0' && sym <= '9') {
+		res = sym - '0';
+	}
+	return res;
+}
+
+int StrToInt(char *str) {
+	int res = 0;
+	for (size_t i = 0; str[i] != '\0'; i++) {
+		res *= 10;
+		res += CharToInt(str[i]);
+	}
+	return res;
+}
+
 class Container {
 public:
 	size_t K;
 	size_t Step;
 	size_t Points;
 	size_t Experiments;
+	size_t ExperimentNum;
 	vector <size_t> Total;
+	int MaxHhreads;
 };
 
 size_t RandSumm(void) {
@@ -24,57 +43,99 @@ size_t RandSumm(void) {
 	return a + b;
 }
 
+void *ExperimentModeller(void *arg) {
+	Container *obj = (Container *) arg;
+
+	size_t i = obj->ExperimentNum;
+	//cout << "ExN: " << i << endl;
+
+	obj->Total[i] = obj->Points;
+	for (size_t j = obj->Step; j < obj->K; j++) {
+		obj->Total[i] += RandSumm();
+	}	
+
+	return arg;
+}
+
 void *ChanseCounter(void *arg) {
+	const size_t MSEC_CONST = 1000;
 	Container *obj = (Container *) arg;
 
 	obj->Total.resize(obj->Experiments);
-	for (size_t i = 0; i < obj->Experiments; i++) {
-		obj->Total[i] = obj->Points;
-		for (size_t j = obj->Step; j < obj->K; j++) {
-			obj->Total[i] += RandSumm();
-		}
+	vector <pthread_t> threads_tmp;
+	threads_tmp.resize(obj->Experiments);
+
+	size_t oldest_thread = 0;
+	size_t threads_started = 0;
+	size_t max_hreads = obj->MaxHhreads;
+	if (max_hreads < 1) {
+		max_hreads = 1;
 	}
-	return (void *) arg;
+	for (size_t i = 0; i < obj->Experiments; i++) {
+		if (threads_started >= obj->MaxHhreads) {
+			pthread_join(threads_tmp[oldest_thread], NULL);
+			oldest_thread++;
+			if (oldest_thread >= obj->Experiments) {
+				oldest_thread = obj->Experiments - 1;
+			}
+			threads_started--;
+		}
+		obj->ExperimentNum = i;
+		pthread_create(&(threads_tmp[i]), NULL, ExperimentModeller, arg);
+		threads_started++;
+		usleep(MSEC_CONST);
+		//sleep(1);
+	}
+
+	return arg;
 }
 
-int main() {
+
+int main(int argc, char* argv[]) {
+	int threads_cnt = 1;
+	if (argc > 1) {
+		threads_cnt = StrToInt(argv[1]);
+	}
 	srand(time(NULL));
-	Container player1, player2;
-	pthread_t thread1, thread2;
+	Container player[2];
+	pthread_t thread[2];
 
 	int thread_result[2];
 	cout << "Input basic data" << endl;
+	player[0].MaxHhreads = threads_cnt;
+	player[1].MaxHhreads = threads_cnt;
 
 	size_t k;
 	cout << "Summary steps: ";
 	cin >> k;
-	player1.K = k;
-	player2.K = k;
+	player[0].K = k;
+	player[1].K = k;
 
 	size_t experiments;
 	cout << "Experiments: ";
 	cin >> experiments;
-	player1.Experiments = experiments;
-	player2.Experiments = experiments;
+	player[0].Experiments = experiments;
+	player[1].Experiments = experiments;
 
 	size_t step;
 	cout << "Current step: ";
 	cin >> step;
-	player1.Step = step;
-	player2.Step = step;
+	player[0].Step = step;
+	player[1].Step = step;
 
 	cout << endl << "Input Player1 data" << endl;
 	cout << "Points: ";
-	cin >> player1.Points;
+	cin >> player[0].Points;
 
 	cout << endl << "Input Player2 data" << endl;
 	cout << "Points: ";
-	cin >> player2.Points;
+	cin >> player[1].Points;
 
 	cout << endl << "Modeling games..." << endl;
-	thread_result[0] = pthread_create(&thread1, NULL, ChanseCounter, &player1);
-	thread_result[1] = pthread_create(&thread2, NULL, ChanseCounter, &player2);
-	sleep(1);
+	thread_result[0] = pthread_create(&(thread[0]), NULL, ChanseCounter, &player[0]);
+	thread_result[1] = pthread_create(&(thread[1]), NULL, ChanseCounter, &player[1]);
+	pthread_join(thread[0], NULL);
+	pthread_join(thread[1], NULL);
 
 	if (thread_result[1] != 0 || thread_result[1] != 0) {
 		cout << "ERROR: Modeling games. Aborting..." << endl;
@@ -85,9 +146,9 @@ int main() {
 	cout << endl << "Counting chanses..." << endl;
 	size_t point_summs[] = {0, 0};
 	for (size_t i = 0; i < experiments; i++) {
-		if (player1.Total[i] > player2.Total[i]) {
+		if (player[0].Total[i] > player[1].Total[i]) {
 			point_summs[0]++;
-		} else if (player1.Total[i] < player2.Total[i]) {
+		} else if (player[0].Total[i] < player[1].Total[i]) {
 			point_summs[1]++;
 		}
 	}
@@ -99,6 +160,10 @@ int main() {
 
 	cout << "=============================" << endl;
 	cout << "RESULTS" << endl;
+	for (size_t i = 0; i < experiments; i++) {
+		cout << player[0].Total[i] << '\t' << player[1].Total[i] << endl;
+	}
+	cout << endl;
 	cout << "Player1 chanse: " << chanses[0] << "%" << endl;
 	cout << "Player2 chanse: " << chanses[1] << "%" << endl;
 	cout << endl;
